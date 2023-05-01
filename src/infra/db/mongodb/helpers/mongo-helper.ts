0de1e614b00abd963c04cc51c274dad81459290b
Alessandro Acer <alex.sandro.jb@hotmail.com>
2023-05-01 16:37:37 -0300

    fix: corrigindo os testes

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -16,7 +16,8 @@ export const MongoHelper = {
   },
 
   map: (collection: any): any => {
-    const { _id, ...collectionWithoutId } = collection
-    return Object.assign({}, collectionWithoutId, { id: _id })
+    console.log('collection', collection)
+    const { acknowledged, insertedId } = collection
+    return Object.assign(acknowledged, insertedId)
   }
 }
