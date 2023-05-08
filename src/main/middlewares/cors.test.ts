4564f54d01294a69a5b58f7109ab98e617fb2a2a
Alessandro Acer <alex.sandro.jb@hotmail.com>
2023-05-08 13:52:14 -0300

    feat: setando tipos de retorno

---
+++ src/main/middlewares/cors.test.ts
@@ -0,0 +1,16 @@
+import  request from "supertest"
+import app from "../config/app"
+
+describe('CORS Middleware', () => {
+    test('Should enable CORS', async () => {
+        app.post('/test_cors', (req, res) => {
+            res.send()
+        })
+
+        await request(app)
+            .get('/test_cors')
+            .expect('access-cors-allow-origin', '*')
+            .expect('access-cors-allow-methods', '*')
+            .expect('access-cors-allow-headers', '*')
+    })
+} )
