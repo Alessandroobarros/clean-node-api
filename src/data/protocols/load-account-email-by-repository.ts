86c0ceb0fdb346845b641e42a93e9acc86eaba02
Alessandro Acer <alex.sandro.jb@hotmail.com>
2023-10-03 21:03:02 -0300

    feat: ensure SbAuthentication calls LoadAccountByEmail

---
+++ src/data/protocols/load-account-email-by-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../usecases/add-Account/db-add-account-protocols'
+
+export interface LoadAccountByEmailRepository {
+  load: (email: string) => Promise<AccountModel>
+}
